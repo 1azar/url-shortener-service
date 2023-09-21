059c2e57426bbe0f3047411fcfa7c988c1be1771
1azar <github.com/1azar>
2023-09-21 18:22:10 +0900

    feat (logger): engage logger

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,8 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"log/slog"
+	"os"
 	config "url-shortener/internal"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
@@ -18,9 +27,12 @@ func run() error {
 		return err
 	}
 
-	println(cfg) //TODO: DELETE ME
-
-	//TODO: logger
+	log, err := setupLogger(cfg.Env)
+	if err != nil {
+		return err
+	}
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	//TODO: storage
 
@@ -29,4 +41,24 @@ func run() error {
 	//TODO: start server
 
 	return nil
+}
+
+func setupLogger(env string) (*slog.Logger, error) {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	default:
+		return nil, fmt.Errorf("uknown env mode: %s", env)
+	}
+
+	return log, nil
 }
